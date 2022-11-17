d7458c94ecf5fd8a2a3d8861ceb3ab659dac2a9d
lstnchDev <freest2334@gmail.com>
2022-11-17 20:08:53 +0300

    ref

--- js/script.js
+++ js/script.js
@@ -1,17 +1,26 @@
+
 const observer = new IntersectionObserver(entries =>{
     entries.forEach(entry =>{
         //если элемент появляется и класс соотвествует соотевтсвующему анимированному классу
-        if(entry.isIntersecting && ("work".includes(entry.target.className) || "skills-items".includes(entry.target.className))){
+        if(entry.isIntersecting && ("work".includes(entry.target.className) || "skills-left".includes(entry.target.className))){
             entry.target.classList.add("portfolio-anim")
         }
         if(entry.isIntersecting && "contact".includes(entry.target.className)){
             entry.target.classList.add("contact-anim")
+        }
+        if(entry.isIntersecting && 'skill-right'.includes(entry.target.className)){
+            entry.target.classList.add("skill-right-anim")
+        }
+        if(entry.isIntersecting && 'skill-left'.includes(entry.target.className)){
+            entry.target.classList.add("skill-left-anim")
         }
     })
 })
 
 const works = document.querySelectorAll('.work')
 const contacts = document.querySelectorAll('.contact')
+const skillRight = document.querySelectorAll('.skill-right')
+const skillLeft = document.querySelectorAll('.skill-left')
 
 
 //создаем наблюдателей и сообщаем какие элементы нужно отслеживать
@@ -20,6 +29,12 @@ for (let work of works){
 }
 for (let contact of contacts){
     observer.observe(contact)
+}
+for (let skill of skillLeft){
+    observer.observe(skill)
+}
+for (let skill of skillRight){
+    observer.observe(skill)
 }
 observer.observe(document.querySelector('.work'))
 observer.observe(document.querySelector('.skills-items'))
